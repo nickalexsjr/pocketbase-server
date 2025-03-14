87fa353f5f973138a22fa49909144491146ec1d6
nickalexsjr <nickalexs@hotmail.com>
2025-03-15 00:10:03 +1100

    Added PocketBase files

---
+++ pb_migrations/1741948076_updated_users.js
@@ -0,0 +1,30 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "authRule": null,
+    "createRule": null,
+    "deleteRule": null,
+    "listRule": null,
+    "updateRule": null,
+    "viewRule": null
+  }, collection)
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "authRule": "",
+    "createRule": "",
+    "deleteRule": "id = @request.auth.id",
+    "listRule": "id != \"\"",
+    "updateRule": "id = @request.auth.id",
+    "viewRule": "id != \"\""
+  }, collection)
+
+  return app.save(collection)
+})
